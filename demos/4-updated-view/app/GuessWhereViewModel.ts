20b50fad6d289d160572c06a5b48146047fd2d92
Matt Driscoll <MDriscoll@esri.com>
2019-03-04 11:21:14 -0800

    starting point

---
+++ demos/4-updated-view/app/GuessWhereViewModel.ts
@@ -0,0 +1,40 @@
+/// <amd-dependency path="esri/core/tsSupport/declareExtendsHelper" name="__extends" />
+/// <amd-dependency path="esri/core/tsSupport/decorateHelper" name="__decorate" />
+
+import MapView = require("esri/views/MapView");
+import SceneView = require("esri/views/SceneView");
+
+import Accessor = require("esri/core/Accessor");
+
+import { declared, property, subclass } from "esri/core/accessorSupport/decorators";
+
+interface GuessWhereViewModelProperties {
+  view: MapView | SceneView;
+}
+
+@subclass("esri.demo.GuessWhereViewModel")
+class GuessWhereViewModelFinal extends declared(Accessor) {
+  //--------------------------------------------------------------------------
+  //
+  //  Lifecycle
+  //
+  //--------------------------------------------------------------------------
+
+  constructor(props?: GuessWhereViewModelProperties) {
+    super();
+  }
+
+  //--------------------------------------------------------------------------
+  //
+  //  Properties
+  //
+  //--------------------------------------------------------------------------
+
+  //--------------------------------------------------------------------------
+  //
+  //  Public Methods
+  //
+  //--------------------------------------------------------------------------
+}
+
+export = GuessWhereViewModelFinal;
