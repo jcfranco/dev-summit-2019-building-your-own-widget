43a2427f1caf1a72394327c392d8e104b320c46a
Matt Driscoll <MDriscoll@esri.com>
2019-03-04 15:21:01 -0800

    update

--- demos/4-updated-view/VM-STEPS.md
+++ demos/4-updated-view/VM-STEPS.md
@@ -4,27 +4,16 @@
 
 Lets create our game widget by first creating the ViewModel.
 
-## Add properties
+## Rename class
 
 ```ts
-//----------------------------------
-//  choices
-//----------------------------------
+@subclass("esri.demo.GuessWhereViewModel")
+class GuessWhereViewModel extends declared(Accessor) {
+```
 
-@property({
-  readOnly: true
-})
-readonly choices: Choices = null;
+## Add state property
 
-//----------------------------------
-//  points
-//----------------------------------
-
-@property({
-  readOnly: true
-})
-readonly points: number = 0;
-
+```ts
 //----------------------------------
 //  state
 //----------------------------------
@@ -35,20 +24,6 @@ readonly points: number = 0;
 get state(): State {
   return !this._active ? "splash" : "playing";
 }
-
-//----------------------------------
-//  view
-//----------------------------------
-
-@property() view: MapView | SceneView = null;
-```
-
-## Import interfaces
-
-View & import interfaces for Choice and Choices
-
-```ts
-import { Choice, Choices } from "./interfaces";
 ```
 
 ## Create State type
@@ -67,7 +42,7 @@ private _active: boolean = false;
 
 Compile and view the [vm-test.html](vm-test.html) page
 
-## Add `start()` method
+## Modify `start()` method to update `_active`
 
 ```ts
 start(): void {
@@ -78,112 +53,9 @@ start(): void {
 }
 ```
 
-## Add `_setNextchoices()` private method
+## Modify `end` public method to update state
 
 ```ts
-private _setNextChoices(): void {
-  const choices = pickChoices(this._choices);
-
-  this._correctIndex = Math.floor(Math.random() * 2);
-  this._set("choices", choices);
-  this._goToChoice(choices);
-}
-```
-
-## Import `pickChoices()` utility method
-
-```ts
-import { pickChoices } from "./utils";
-```
-
-## Add `_choices` private variable
-
-```ts
-private _choices: Choice[] = [
-  {
-    name: "Palm Springs",
-    feature: new Graphic({
-      geometry: {
-        type: "point",
-        x: -12970052.058526255,
-        y: 4004544.8553683264,
-        spatialReference: { wkid: 102100 }
-      } as Point,
-      symbol: { type: "simple-marker" } as SimpleMarkerSymbol
-    })
-  },
-  {
-    name: "Redlands",
-    feature: new Graphic({
-      geometry: {
-        type: "point",
-        x: -13044706.248636946,
-        y: 4035952.8114616736,
-        spatialReference: { wkid: 102100 }
-      } as Point,
-      symbol: { type: "simple-marker" } as SimpleMarkerSymbol
-    })
-  },
-  {
-    name: "San Diego",
-    feature: new Graphic({
-      geometry: {
-        type: "point",
-        x: -13042381.897669187,
-        y: 3856726.5654889513,
-        spatialReference: { wkid: 102100 }
-      } as Point,
-      symbol: { type: "simple-marker" } as SimpleMarkerSymbol
-    })
-  }
-];
-```
-
-## Add choices imports
-
-```ts
-import Graphic = require("esri/Graphic");
-import Point = require("esri/geometry/Point");
-import SimpleMarkerSymbol = require("esri/symbols/SimpleMarkerSymbol");
-```
-
-## Add `_correctIndex` private variable
-
-```ts
-private _correctIndex: number = null;
-```
-
-## Add `_goToChoice` private method
-
-```ts
-private _goToChoice(choices: Choices): void {
-  if (!choices) {
-    return;
-  }
-  const correct = this.choices[this._correctIndex];
-
-  const { view } = this;
-  (view as any).goTo(correct.feature, { animate: false });
-  view.graphics.removeAll();
-  view.graphics.add(correct.feature);
-}
-```
-
-## Add `choose` and `end` public methods
-
-```ts
-choose(choice: Choice): boolean {
-  const correct = this.choices[this._correctIndex] === choice;
-
-  if (correct) {
-    this._set("points", this.points + 1);
-  }
-
-  this._setNextChoices();
-
-  return correct;
-}
-
 end() {
   this._active = false;
   this.notifyChange("state");
--- demos/4-updated-view/app/GuessWhereViewModel.ts
+++ demos/4-updated-view/app/GuessWhereViewModel.ts
@@ -5,22 +5,26 @@ import MapView = require("esri/views/MapView");
 import SceneView = require("esri/views/SceneView");
 
 import Accessor = require("esri/core/Accessor");
-
+import Graphic = require("esri/Graphic");
+import Point = require("esri/geometry/Point");
+import SimpleMarkerSymbol = require("esri/symbols/SimpleMarkerSymbol");
 import { declared, property, subclass } from "esri/core/accessorSupport/decorators";
+import { Choice, Choices } from "./interfaces";
+import { pickChoices } from "./utils";
 
-interface GuessWhereViewModelProperties {
+interface CustomClassProperties {
   view: MapView | SceneView;
 }
 
-@subclass("esri.demo.GuessWhereViewModel")
-class GuessWhereViewModel extends declared(Accessor) {
+@subclass("esri.demo.CustomClass")
+class CustomClass extends declared(Accessor) {
   //--------------------------------------------------------------------------
   //
   //  Lifecycle
   //
   //--------------------------------------------------------------------------
 
-  constructor(props?: GuessWhereViewModelProperties) {
+  constructor(props?: CustomClassProperties) {
     super();
   }
 
@@ -30,11 +34,76 @@ class GuessWhereViewModel extends declared(Accessor) {
   //
   //--------------------------------------------------------------------------
 
+  private _choices: Choice[] = [
+    {
+      name: "Palm Springs",
+      feature: new Graphic({
+        geometry: {
+          type: "point",
+          x: -12970052.058526255,
+          y: 4004544.8553683264,
+          spatialReference: { wkid: 102100 }
+        } as Point,
+        symbol: { type: "simple-marker" } as SimpleMarkerSymbol
+      })
+    },
+    {
+      name: "Redlands",
+      feature: new Graphic({
+        geometry: {
+          type: "point",
+          x: -13044706.248636946,
+          y: 4035952.8114616736,
+          spatialReference: { wkid: 102100 }
+        } as Point,
+        symbol: { type: "simple-marker" } as SimpleMarkerSymbol
+      })
+    },
+    {
+      name: "San Diego",
+      feature: new Graphic({
+        geometry: {
+          type: "point",
+          x: -13042381.897669187,
+          y: 3856726.5654889513,
+          spatialReference: { wkid: 102100 }
+        } as Point,
+        symbol: { type: "simple-marker" } as SimpleMarkerSymbol
+      })
+    }
+  ];
+
+  private _correctIndex: number = null;
+
   //--------------------------------------------------------------------------
   //
   //  Properties
   //
   //--------------------------------------------------------------------------
+
+  //----------------------------------
+  //  choices
+  //----------------------------------
+
+  @property({
+    readOnly: true
+  })
+  readonly choices: Choices = null;
+
+  //----------------------------------
+  //  points
+  //----------------------------------
+
+  @property({
+    readOnly: true
+  })
+  readonly points: number = 0;
+
+  //----------------------------------
+  //  view
+  //----------------------------------
+
+  @property() view: MapView | SceneView = null;
 
   //--------------------------------------------------------------------------
   //
@@ -42,11 +111,53 @@ class GuessWhereViewModel extends declared(Accessor) {
   //
   //--------------------------------------------------------------------------
 
+  start(): void {
+    this._setNextChoices();
+    this._set("points", 0);
+  }
+
+  choose(choice: Choice): boolean {
+    const correct = this.choices[this._correctIndex] === choice;
+
+    if (correct) {
+      this._set("points", this.points + 1);
+    }
+
+    this._setNextChoices();
+
+    return correct;
+  }
+
+  end() {
+    this.view.graphics.removeAll();
+  }
+
   //--------------------------------------------------------------------------
   //
   //  Private Methods
   //
   //--------------------------------------------------------------------------
+
+  private _goToChoice(choices: Choices): void {
+    if (!choices) {
+      return;
+    }
+
+    const correct = choices[this._correctIndex];
+
+    const { view } = this;
+    (view as any).goTo(correct.feature, { animate: false });
+    view.graphics.removeAll();
+    view.graphics.add(correct.feature);
+  }
+
+  private _setNextChoices(): void {
+    const choices = pickChoices(this._choices);
+
+    this._correctIndex = Math.floor(Math.random() * 2);
+    this._set("choices", choices);
+    this._goToChoice(choices);
+  }
 }
 
-export = GuessWhereViewModel;
+export = CustomClass;
